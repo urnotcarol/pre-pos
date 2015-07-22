06f8f7f3fcf155b333767515b7f47dc11afa1869
Chrissie <urnotchrissie@outlook.com>
2015-07-22 14:14:30 +0800

    Commit section-3 - practice-2;

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,3 +1,10 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  collection_a.forEach(function(val) {
+    for(var i = 0; i < object_b.value.length; i++) {
+      if(val.key === object_b.value[i]) {
+        val.count = val.count - (val.count - (val.count % 3)) / 3;
+      }
+      }
+    });
+  return collection_a;
 }
